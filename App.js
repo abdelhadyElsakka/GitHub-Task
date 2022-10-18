bf3fe05fd8a79b7578e28de907d7988657019add
sakka <abdelhadyfathalla@gmail.com>
2022-10-18 17:08:44 +0200

    add redux toolkit and react navigation and pagination

--- App.js
+++ App.js
@@ -1,30 +1,26 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import store from './redux/store';
 // Screens
 import ExploreScreen from './Screens/ExploreScreen';
 import RepositoriesScreen from './Screens/RepositoriesScreen';
-
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
     <Provider store={store}>
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-        }}>
-        <Stack.Screen name="Explore" component={ExploreScreen} />
-        <Stack.Screen
-          name="Repositories"
-          component={RepositoriesScreen}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerShown: false,
+          }}>
+          <Stack.Screen name="Explore" component={ExploreScreen} />
+          <Stack.Screen name="Repositories" component={RepositoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 };
